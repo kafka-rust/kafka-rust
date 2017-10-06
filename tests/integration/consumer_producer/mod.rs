7815b1d7629fcfc9530d15ba14731bb41f32ba00
Skyler Hawthorne <skylerhawthorne@gmail.com>
2017-10-06 16:34:55 -0700

    Make sure consumer is always at the latest offset

--- tests/integration/consumer_producer/mod.rs
+++ tests/integration/consumer_producer/mod.rs
@@ -1,13 +1,14 @@
 pub use super::*;
 use kafka::producer::{Producer, RequiredAcks};
 use kafka;
+use kafka::client::FetchOffset;
 use kafka::consumer::Consumer;
 use std::time::Duration;
 
 pub fn test_producer() -> Producer {
     Producer::from_hosts(vec![LOCAL_KAFKA_BOOTSTRAP_HOST.to_owned()])
         .with_ack_timeout(Duration::from_secs(1))
-        .with_required_acks(RequiredAcks::One)
+        .with_required_acks(RequiredAcks::All)
         .create()
         .unwrap()
 }
@@ -23,7 +24,25 @@ pub fn test_consumer_builder() -> kafka::consumer::Builder {
 
 /// Return a ready Kafka consumer with all default settings
 pub fn test_consumer() -> Consumer {
-    test_consumer_builder().create().unwrap()
+    let mut consumer = test_consumer_builder().create().unwrap();
+    let topics = [TEST_TOPIC_NAME, TEST_TOPIC_NAME_2];
+
+    // Fetch the latest offsets and commit those so that this consumer
+    // is always at the latest offset before being used.
+    {
+        let client = consumer.client_mut();
+        let latest_offsets = client.fetch_offsets(&topics, FetchOffset::Latest).unwrap();
+
+        for (topic, partition_offsets) in latest_offsets {
+            for po in partition_offsets {
+                client
+                    .commit_offset(TEST_GROUP_NAME, &topic, po.partition, po.offset)
+                    .unwrap();
+            }
+        }
+    }
+
+    consumer
 }
 
 mod producer;
