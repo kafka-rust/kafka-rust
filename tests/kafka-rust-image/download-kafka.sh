10a08a37c985e27a4dfc940ac3ccc7996c9786da
Skyler Hawthorne <skyler@opendns.com>
2019-09-10 08:40:38 -0700

    Merge xitep's changes to Docker stuff

--- tests/kafka-rust-image/download-kafka.sh
+++ tests/kafka-rust-image/download-kafka.sh
@@ -2,6 +2,5 @@
 
 set -ex
 
-mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
-url="${mirror}kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
-curl -Sso "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz" "${url}" 
+url="https://archive.apache.org/dist/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+curl -Sso "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz" "${url}"
