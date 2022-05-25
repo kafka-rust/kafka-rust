2f5c71123e832b56bcef269a9fd46532f66af556
Midnight Exigent <36641328+midnightexigent@users.noreply.github.com>
2022-05-25 22:43:26 +0200

    Update

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,45 +1,40 @@
-use std::io::{Read, Write};
+use std::borrow::Cow;
 use std::mem;
 use std::time::Duration;
 
-use crate::codecs::{FromByte, ToByte};
 use crate::error::{Error, KafkaCode, Result};
 
+mod api_key;
 pub mod consumer;
-pub mod decoder;
+mod decoder;
+mod error;
 pub mod metadata;
 pub mod offset;
 pub mod produce;
 
 pub mod fetch;
-mod zreader;
 
+use self::api_key::ApiKey;
 // ~ convenient re-exports for request/response types defined in the
 // submodules
 pub use self::consumer::{
     GroupCoordinatorRequest, GroupCoordinatorResponse, OffsetCommitRequest, OffsetCommitResponse,
     OffsetCommitVersion, OffsetFetchRequest, OffsetFetchResponse, OffsetFetchVersion,
 };
+use self::decoder::Decoder;
 pub use self::fetch::FetchRequest;
 pub use self::metadata::{MetadataRequest, MetadataResponse};
 pub use self::offset::{OffsetRequest, OffsetResponse};
 pub use self::produce::{ProduceRequest, ProduceResponse};
 
-// --------------------------------------------------------------------
-
-const API_KEY_PRODUCE: i16 = 0;
-const API_KEY_FETCH: i16 = 1;
-const API_KEY_OFFSET: i16 = 2;
-const API_KEY_METADATA: i16 = 3;
-// 4-7 reserved for non-public kafka api services
-const API_KEY_OFFSET_COMMIT: i16 = 8;
-const API_KEY_OFFSET_FETCH: i16 = 9;
-const API_KEY_GROUP_COORDINATOR: i16 = 10;
-
 // the default version of Kafka API we are requesting
 const API_VERSION: i16 = 0;
 
 // --------------------------------------------------------------------
+
+pub trait Decode<'de> {
+    fn decode(decoder: &mut Decoder<'de>) -> Result<Self, decoder::Error>;
+}
 
 /// Provides a way to parse the full raw response data into a
 /// particular response structure.
@@ -50,93 +45,22 @@ pub trait ResponseParser {
 
 // --------------------------------------------------------------------
 
-impl KafkaCode {
-    fn from_protocol(n: i16) -> Option<KafkaCode> {
-        if n == 0 {
-            return None;
-        }
-        if n >= KafkaCode::OffsetOutOfRange as i16 && n <= KafkaCode::UnsupportedVersion as i16 {
-            return Some(unsafe { mem::transmute(n as i8) });
-        }
-        Some(KafkaCode::Unknown)
-    }
-}
-
-#[test]
-fn test_kafka_code_from_protocol() {
-    use std::i16;
-
-    macro_rules! assert_kafka_code {
-        ($kcode:path, $n:expr) => {
-            assert!(if let Some($kcode) = KafkaCode::from_protocol($n) {
-                true
-            } else {
-                false
-            })
-        };
-    }
-
-    assert!(KafkaCode::from_protocol(0).is_none());
-    assert_kafka_code!(
-        KafkaCode::OffsetOutOfRange,
-        KafkaCode::OffsetOutOfRange as i16
-    );
-    assert_kafka_code!(
-        KafkaCode::IllegalGeneration,
-        KafkaCode::IllegalGeneration as i16
-    );
-    assert_kafka_code!(
-        KafkaCode::UnsupportedVersion,
-        KafkaCode::UnsupportedVersion as i16
-    );
-    assert_kafka_code!(KafkaCode::Unknown, KafkaCode::Unknown as i16);
-    // ~ test some un mapped non-zero codes; should all map to "unknown"
-    assert_kafka_code!(KafkaCode::Unknown, i16::MAX);
-    assert_kafka_code!(KafkaCode::Unknown, i16::MIN);
-    assert_kafka_code!(KafkaCode::Unknown, -100);
-    assert_kafka_code!(KafkaCode::Unknown, 100);
-}
-
-// a (sub-) module private method for error
-impl Error {
-    fn from_protocol(n: i16) -> Option<Error> {
-        KafkaCode::from_protocol(n).map(Error::Kafka)
-    }
-}
-
-// --------------------------------------------------------------------
-
 #[derive(Debug)]
 pub struct HeaderRequest<'a> {
-    pub api_key: i16,
+    pub api_key: ApiKey,
     pub api_version: i16,
     pub correlation_id: i32,
     pub client_id: &'a str,
 }
 
-impl<'a> HeaderRequest<'a> {
-    fn new(
-        api_key: i16,
-        api_version: i16,
-        correlation_id: i32,
-        client_id: &'a str,
-    ) -> HeaderRequest<'_> {
-        HeaderRequest {
-            api_key,
-            api_version,
-            correlation_id,
-            client_id,
-        }
-    }
-}
-
-impl<'a> ToByte for HeaderRequest<'a> {
-    fn encode<W: Write>(&self, buffer: &mut W) -> Result<()> {
-        self.api_key.encode(buffer)?;
-        self.api_version.encode(buffer)?;
-        self.correlation_id.encode(buffer)?;
-        self.client_id.encode(buffer)?;
-        Ok(())
+impl<'de> Decode<'de> for HeaderRequest<'de> {
+    fn decode(decoder: &mut Decoder<'de>) -> Result<Self> {
+        Ok(Self {
+            api_key: decoder.decode_api_key()?,
+            api_version: decoder.decode_int16()?,
+            correlation_id: decoder.decode_int32()?,
+            client_id: decoder.decode_string()?,
+        })
     }
 }
 
@@ -145,15 +69,6 @@ impl<'a> ToByte for HeaderRequest<'a> {
 #[derive(Default, Debug, Clone)]
 pub struct HeaderResponse {
     pub correlation: i32,
-}
-
-impl FromByte for HeaderResponse {
-    type R = HeaderResponse;
-
-    #[allow(unused_must_use)]
-    fn decode<T: Read>(&mut self, buffer: &mut T) -> Result<()> {
-        self.correlation.decode(buffer)
-    }
 }
 
 // --------------------------------------------------------------------
--- src/protocol/decoder.rs
+++ src/protocol/decoder.rs
@@ -1,17 +1,26 @@
-use std::{io, num, result, string};
+use std::{array, num, result, str};
 
-use integer_encoding::{VarInt, VarIntReader};
+use integer_encoding::VarInt;
+use num_enum::TryFromPrimitive;
 use thiserror::Error as ThisError;
 use uuid::Uuid;
 
+use super::api_key::ApiKey;
+
 #[derive(Debug, ThisError)]
 pub enum Error {
-    #[error(transparent)]
-    Io(#[from] io::Error),
+    #[error("unexpected eof")]
+    UnexpectedEof,
     #[error(transparent)]
     TryFromInt(#[from] num::TryFromIntError),
     #[error(transparent)]
-    FromUtf8(#[from] string::FromUtf8Error),
+    Utf8(#[from] str::Utf8Error),
+    #[error(transparent)]
+    TryFromSlice(#[from] array::TryFromSliceError),
+    #[error(transparent)]
+    DecodeApiKey(#[from] num_enum::TryFromPrimitiveError<ApiKey>),
+    #[error("failed to decode var")]
+    DecodeVar,
 }
 
 type Result<T> = result::Result<T, Error>;
@@ -22,45 +31,100 @@ macro_rules! decode_num {
     };
 }
 
-pub struct Decoder<R> {
-    rdr: R,
+pub struct Decoder<'storage> {
+    bytes: &'storage [u8],
 }
 
-// XXX: it would be more efficient to implement using a cust Read that allows to inspect
-// the inner slice [u8]: https://github.com/bincode-org/bincode/blob/6ec69a38b5698876662e93ca928f6f41183611d5/src/de/read.rs
-impl<R: io::Read> Decoder<R> {
-    pub fn new(rdr: R) -> Self {
-        Self { rdr }
+impl<'storage> Decoder<'storage> {
+    pub fn new(bytes: &'storage [u8]) -> Self {
+        Self { bytes }
     }
-    pub fn take_bytes(&mut self, n: usize) -> io::Result<Vec<u8>> {
-        let buf = vec![0; n];
-        self.rdr.read_exact(&mut buf)?;
-        Ok(buf)
+
+    pub fn take_bytes(&mut self, n: usize) -> Result<&'storage [u8]> {
+        if n < self.bytes.len() {
+            let (lhs, rhs) = self.bytes.split_at(n);
+            self.bytes = rhs;
+            Ok(lhs)
+        } else {
+            Err(Error::UnexpectedEof)
+        }
     }
-    pub fn take_bytes_const<const N: usize>(&mut self) -> io::Result<[u8; N]> {
-        let buf = [0; N];
-        self.rdr.read_exact(&mut buf)?;
-        Ok(buf)
+
+    pub fn take_bytes_const<const N: usize>(&mut self) -> Result<[u8; N]> {
+        if N < self.bytes.len() {
+            let (lhs, rhs) = self.bytes.split_at(N);
+            self.bytes = rhs;
+            Ok(lhs.try_into()?)
+        } else {
+            Err(Error::UnexpectedEof)
+        }
     }
-    pub fn decode_var<T: VarInt>(&mut self) -> io::Result<T> {
-        self.rdr.read_varint()
+    pub fn decode_int8(&mut self) -> Result<i8> {
+        decode_num!(self, i8)
+    }
+    pub fn decode_int16(&mut self) -> Result<i16> {
+        decode_num!(self, i16)
+    }
+    pub fn decode_int32(&mut self) -> Result<i32> {
+        decode_num!(self, i32)
+    }
+
+    fn decode_var<T: VarInt>(&mut self) -> Result<T> {
+        if let Some((value, size)) = T::decode_var(self.bytes) {
+            let _ = self.take_bytes(size)?;
+            Ok(value)
+        } else {
+            Err(Error::DecodeVar)
+        }
+    }
+    pub fn decode_varint(&mut self) -> Result<i32> {
+        self.decode_var()
+    }
+    pub fn decode_unsigned_varint(&mut self) -> Result<u32> {
+        self.decode_var()
+    }
+    pub fn decode_varlong(&mut self) -> Result<i64> {
+        self.decode_var()
+    }
+    pub fn decode_unsigned_varlong(&mut self) -> Result<u64> {
+        self.decode_var()
     }
     pub fn decode_uuid(&mut self) -> Result<Uuid> {
         Ok(Uuid::from_bytes(self.take_bytes_const()?))
     }
-    pub fn decode_string(&mut self) -> Result<String> {
-        let len = decode_num!(self, i16)?;
-        Ok(String::from_utf8(self.take_bytes(len.try_into()?)?)?)
+    pub fn decode_string(&mut self) -> Result<&'storage str> {
+        let len = self.decode_int16()?;
+        Ok(str::from_utf8(self.take_bytes(len.try_into()?)?)?)
+    }
+    pub fn decode_compact_string(&mut self) -> Result<&'storage str> {
+        let len: usize = self.decode_unsigned_varint()?.try_into()?;
+        Ok(str::from_utf8(self.take_bytes(len - 1)?)?)
+    }
+    pub fn decode_nullable_string(&mut self) -> Result<Option<&'storage str>> {
+        let len = self.decode_int16()?;
+        if len == -1 {
+            Ok(None)
+        } else {
+            let len: usize = len.try_into()?;
+            Ok(Some(str::from_utf8(self.take_bytes(len)?)?))
+        }
+    }
+    pub fn decode_compact_nullable_string(&mut self) -> Result<Option<&'storage str>> {
+        let len = self.decode_var()?;
     }
     pub fn decode_array<T>(
         &mut self,
-        decode_next_elem: fn(&mut Decoder<R>) -> Result<T>,
+        decode_next_elem: fn(&mut Self) -> Result<T>,
     ) -> Result<Vec<T>> {
-        let len = decode_num!(self, i32)?;
+        let len = self.decode_int32()?;
         let arr = Vec::with_capacity(len.try_into()?);
         for _ in 0..len {
             arr.push(decode_next_elem(self)?);
         }
         Ok(arr)
+    }
+
+    pub fn decode_api_key(&mut self) -> Result<ApiKey> {
+        Ok(ApiKey::try_from_primitive(self.decode_int16()?)?)
     }
 }
